0daa70b73547779d84f0e1942fbe243bf153be1d
SwastyMahaRanii913 <swastymaharani09@gmail.com>
2023-09-21 11:46:32 +0800

    Update akar_kuadrat.php

--- akar_kuadrat.php
+++ akar_kuadrat.php
@@ -1,61 +1,58 @@
 <?php
 // Allow Cross-Origin Resource Sharing (CORS)
-header("Access-Control-Allow-Origin: https://tugas1-ppl.vercel.app, https://tugas1-fodqvqxjp-swastymaharani.vercel.app/");
+header("Access-Control-Allow-Origin: https://tugas1-ppl.vercel.app, https://tugas1-ha8o9gqj6-swastymaharani.vercel.app");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Type");
 
-// // Retrieve database connection information from environment variables
-// $dbHost = getenv('DB_HOST');
-// $dbPort = getenv('DB_PORT');
-// $dbName = getenv('DB_NAME');
-// $dbUser = getenv('DB_USER');
-// $dbPassword = getenv('DB_PASSWORD');
-
-// // Create a database connection
-// $connection = new mysqli($dbHost, $dbUser, $dbPassword, $dbName, $dbPort);
-
-// // Check if the connection was successful
-// if ($connection->connect_error) {
-//     die("Connection failed: " . $connection->connect_error);
-// }
+// Retrieve database connection information from environment variables
+$dbHost = getenv('DB_HOST');
+$dbPort = getenv('DB_PORT');
+$dbName = getenv('DB_NAME');
+$dbUser = getenv('DB_USER');
+$dbPassword = getenv('DB_PASSWORD');
 
 // Function to calculate square root using PHP
-function calculateSquareRoot($number) {
-    return sqrt($number);
+function calculateSquareRoot($numbers) {
+    return sqrt($numbers);
 }
 
 // Handle API requests
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    // Get the 'number' parameter
-    $number = floatval($_GET['number']);
-    echo($number);
-    echo json_encode(['message' => $number]);
+    // Get the 'numbers' parameter
+    $numbers = floatval($_GET['numbers']);
 
-    if ($number !== null) {
-        $squareRoot = calculateSquareRoot($number);
-        echo json_encode(['message' => $squareRoot]);
+    if ($numbers !== null) {
+        $squareRoot = calculateSquareRoot($numbers);
         
-        // // Insert the SQL database with the square root result
-        // $insertSql = "INSERT INTO tb_sqnumbers (numbers, sqnumber) VALUES ($number, $squareRoot)";
-        // echo($insertSql);
-        // if ($connection->query($insertSql) !== TRUE) {
-        //     http_response_code(500); // Internal Server Error
-        //     echo json_encode(['error' => 'Error updating the database']);
-        //     exit; // Exit the script if there's an error
-        // }
-        // else {
-        //     echo json_encode(['square_root' => $squareRoot]);
-        // }
+        // Create a database connection
+        $connection = new mysqli($dbHost, $dbUser, $dbPassword, $dbName);
+
+        // Check if the connection was successful
+        if ($connection->connect_error) {
+            http_response_code(500); // Internal Server Error
+            echo json_encode(['error' => 'Error connecting to the database']);
+            exit; // Exit the script if there's an error
+        }
+
+        // Insert the numbers and square root into the database
+        $insertSql = "INSERT INTO your_table_name (numbers, square_root) VALUES ($numbers, $squareRoot)";
+        
+        if ($connection->query($insertSql) !== TRUE) {
+            http_response_code(500); // Internal Server Error
+            echo json_encode(['error' => 'Error updating the database']);
+            exit; // Exit the script if there's an error
+        } else {
+            echo json_encode(['square_root' => $squareRoot]);
+        }
+
+        // Close the database connection
+        $connection->close();
     } else {
         http_response_code(400); // Bad Request
-        echo json_encode(['error' => 'Missing or invalid "number" parameter']);
+        echo json_encode(['error' => 'Missing or invalid "numbers" parameter']);
     }
 } else {
     http_response_code(405); // Method Not Allowed
     echo json_encode(['error' => 'Method not allowed']);
 }
-
-// Close the database connection
-// $connection->close();
 ?>
-
